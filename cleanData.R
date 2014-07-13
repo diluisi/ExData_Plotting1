f1b119afbc53700244404650ef2e83a4e3793e62
Diego <luiz.diego5@gmail.com>
2014-07-12 22:38:07 -0300

    Update cleanData.R

--- cleanData.R
+++ cleanData.R
@@ -1,9 +1,10 @@
 cleanData <- function() {
+	##This function download the file from website and clean data
 	nameFile<-"clean_data.csv"  
 	data <- download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="household_power_consumption.zip", method="curl")
 	unzipData<-unzip("household_power_consumption.zip")
 	cleanData <- read.table(unzipData, header=T, sep=';', na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 	cleanData <- cleanData[(cleanData$Date == "1/2/2007") | (cleanData$Date == "2/2/2007"),]
 	cleanData$DateTime <- strptime(paste(cleanData$Date, cleanData$Time), "%d/%m/%Y %H:%M:%S")
-	write.csv(cleanData, nameFile)
+	write.csv(cleanData, nameFile) ##creates a new .csv file for plotting
 }
