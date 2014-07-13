e81fdd7cfce0e14d9fdc5e1908b4cdd2fb3ef280
Diego <luiz.diego5@gmail.com>
2014-07-13 17:02:30 -0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
-source("cleandata.R")
+source("cleandata.R") #Load function to clean data
 plot1 <- function() {
-    data <- cleanData()
+    data <- cleanData() #Call function to clean data
     png(filename = "plot1.png", width = 480, height = 480, units = "px")
     hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
     dev.off()
