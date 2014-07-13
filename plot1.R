8e9f52eaa653557ffa7c1cd3102a6cc906c5441e
Diego <luiz.diego5@gmail.com>
2014-07-13 15:48:05 -0300

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,9 @@
+source("cleandata.R")
+plot1 <- function() {
+    data <- cleanData()
+    png(filename = "plot1.png", width = 480, height = 480, units = "px")
+    hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+    dev.off()
+}
+
+plot1()
