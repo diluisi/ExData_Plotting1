591e5872cfd3f610ffc9d82aa4cd73a7a908bd39
Diego <luiz.diego5@gmail.com>
2014-07-13 16:29:27 -0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 source("cleandata.R")
 plot2 <- function() {
     data <- cleanData()
-    png(filename = "plot1.png", width = 480, height = 480, units = "px")
+    png(filename = "plot2.png", width = 480, height = 480, units = "px")
     plot(data$DateTime, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
     dev.off()
 }
